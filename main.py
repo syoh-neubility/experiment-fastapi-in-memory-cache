de640a74254c1e47ddb7b179f13eb0d2ab30ec27
Seung-Yoon Oh <syoh@neubility.co.kr>
2023-02-02 14:16:32 +0900

    fastapi_cache의 구현체를 통해 inmemory storage 사용하도록 변경

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+from typing import Union
 from fastapi import FastAPI
+
+from fastapi_cache.backends.inmemory import InMemoryBackend
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
+async def root():
     return "SUCCESS"
 
 
 in_memory_dict = {}
+in_memory_storage = InMemoryBackend()
 
 
-def get_value_from_in_memory(key: str):
+async def get_value_from_in_memory(key: str):
     global in_memory_dict
 
     if key in in_memory_dict:
@@ -19,14 +23,35 @@ def get_value_from_in_memory(key: str):
 
     return None
 
+
 def update_value_from_in_memory(key: str, value):
     global in_memory_dict
     in_memory_dict[key] = value
 
 
 @app.put("/counts/increment/global-variable")
-def increment_count():
+async def increment_count_global_variable():
     count = get_value_from_in_memory("count")
     next_val = count + 1 if count else 1
     update_value_from_in_memory("count", next_val)
     return next_val
+
+
+
+async def get_count():
+    return await in_memory_storage.get("count")
+
+async def update_count(value: int):
+    print("update_count called with: ", value)
+    return await in_memory_storage.set("count", value, 1)
+
+
+@app.put("/counts/increment/in-memory-cache")
+async def increment_count_in_memory_cache():
+    value = await get_count()
+    print(value)
+    next_value = value + 1 if value else 1
+    await update_count(next_value)
+    value = await get_count()
+    print(value)
+    return next_value
